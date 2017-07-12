c06d3ba1a7d98288935a08f23ca7345bbce7b4f8
tanulo13@athenepraxis.local <tanulo13@athenepraxis.local>
2017-07-12 19:51:58 +0200

    Obi Van és MaceWindu hozzáadva

---
+++ StarWars/DarthMaul.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars
+{
+    class DarthMaul : Sith
+    {
+
+        public override string Name { get; } = "Darth Maul";
+        public override bool IsLightSide { get; } = false;
+
+        public DarthMaul(int power) : base(power)
+        {
+        }
+
+        public override void OnJoinBattle(BattleSimulator simulator)
+        {
+                simulator.LightSide.Morale -= 3;
+                Console.WriteLine($"Az ellenséges Jedi csapata új morálja 3 ponttal csökken: {simulator.LightSide.Morale}");
+        }            
+      
+        public override void PostCombatEffect(BattleSimulator simulator)
+        {
+            
+        }
+
+        public override void PreCombatEffect(BattleSimulator simulator)
+        {
+            
+        }
+
+        public override void OnLeaveBattle(BattleSimulator simulator)
+        {
+
+        }
+    }
+}
---
+++ StarWars/MaceWindu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars
+{
+    class MaceWindu : Jedi
+    {
+        public override string Name { get; } = "Mace Windu";
+        public override bool IsLightSide { get; } = true;
+
+        public MaceWindu(int power) : base (power)
+        {
+
+        }
+
+        public override void OnJoinBattle(BattleSimulator simulator)
+        {
+         
+        }
+
+        public override void PreCombatEffect(BattleSimulator simulator)
+        {
+     
+        }
+
+        public override void PostCombatEffect(BattleSimulator simulator)
+        {
+       
+        }
+
+        public override void OnLeaveBattle(BattleSimulator simulator)
+        {
+            var opponent = simulator
+                .DarkSide // or light side
+                .GetWarrior(simulator);
+            if (Power <= 0 && opponent.Power > 0) // vagy power ==0?
+                opponent.DecreasePower(simulator, 1);
+        }
+    }
+}
