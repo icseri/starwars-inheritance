7c31c3f2b560577ae90b78a46d9dbfcc38ac47ef
KARPATI Zoltan <karpatzo@nead.danet>
2017-07-12 19:18:46 +0200

    Boba Fett

---
+++ StarWars/Warriors/Boba_Fett.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars.Warriors
+{
+    class Boba_Fett : Warrior
+    {
+        public override string Name { get; } = "Boba Fett";
+        public override bool IsLightSide { get; } = true;
+
+        public Boba_Fett(int power) : base(power)
+        {
+        }
+        public override void OnJoinBattle(BattleSimulator simulator)
+        {
+            if (simulator.LightSide.Morale < simulator.DarkSide.Morale)
+            {
+                simulator.DarkSide.Morale += 2;
+                Console.WriteLine($"A csapata új morálja: {simulator.DarkSide.Morale}");
+            }         
+        }
+
+        public override void OnLeaveBattle(BattleSimulator simulator)
+        {
+
+        }
+
+        public override void PostCombatEffect(BattleSimulator simulator)
+        {
+
+        }
+
+        public override void PreCombatEffect(BattleSimulator simulator)
+        {
+
+        }
+    }
+}
